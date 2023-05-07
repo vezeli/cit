b26a58c48b77da12d42afdfee3dd4aed1804ce55
Velibor Zeli <velibor@mech.kth.se>
2023-05-07 23:57:21 +0200

    Add an argument to CLI

--- tax.py
+++ tax.py
@@ -1,3 +1,5 @@
+import sys
+
 from _config import Config
 from calculation import calculate_statistics, format_table
 from data import read_in_transactions
@@ -5,10 +7,15 @@ from data import read_in_transactions
 config = Config()
 
 if __name__ == "__main__":
+    try:
+        financial_year = int(sys.argv[1])
+    except TypeError as e:
+        raise e("Argument `financial_year` must be integer.")
+
     format_table(
         *calculate_statistics(
             df=read_in_transactions(config),
-            financial_year=2021,
+            financial_year=financial_year,
             c=config
         )
     )
