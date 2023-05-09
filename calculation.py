bf044758a85b129ebbd5d8a646dc66023fb5b459
Velibor Zeli <velibor@mech.kth.se>
2023-05-09 23:40:00 +0200

    Correct implementation for `_ACQUISITION_COST`

--- calculation.py
+++ calculation.py
@@ -1,26 +1,70 @@
 from datetime import datetime
+from numbers import Real as R
 
-import pandas as DataFrame
+import numpy as np
+from pandas import concat, DataFrame, Series
 
 from _config import Config
 
 
-def calculate_cost(df: DataFrame, c: Config) -> DataFrame:
-    return df.assign(
-        _cost = lambda df: (
-            -1 * df[c._AMOUNT] * df[c._PRICE] * df[c._FX_RATE]
-        )
-    )
-
-
-def format_table(recieved, aquisition_price, profit_and_loss) -> None:
-    out = f"| RECIEVED: {recieved:.2f} | AQUISITION PRICE: {aquisition_price:.2f} | P&L: {profit_and_loss:.2f} |"
+def format_table(recieved, acquisition_price, profit_and_loss) -> None:
+    out = f"| RECIEVED: {recieved:.2f} | AQUISITION PRICE: {acquisition_price:.2f} | P&L: {profit_and_loss:.2f} |"
 
     print(len(out) * "-")
     print(out)
     print(len(out) * "-")
 
     return None
+
+
+def calculate_acquisition_cost(df_: DataFrame, c: Config) -> DataFrame:
+    _COST = "_cost"
+
+
+    def calculate_cost(df_: DataFrame, c: Config) -> DataFrame:
+        nonlocal _COST
+        df = (
+            df_
+            .copy()
+            .assign(Cost = lambda x: -1*np.sign(x[c._AMOUNT])*x[c._PRICE])
+            .rename(columns={"Cost": _COST})
+        )
+        return df
+
+
+    def _calculate_acquisition_cost(df_: DataFrame, c: Config) -> DataFrame:
+        nonlocal _COST
+
+        def sell(transaction_cost: R) -> bool:
+            return True if transaction_cost > 0 else False
+
+        acquisition_costs = []
+        for _, s in (df := df_.pipe(calculate_cost, c=c)).iterrows():
+            transaction_cost, transaction_amount = s[_COST], s[c._AMOUNT]
+
+            if s.equals(df.iloc[0, :]):
+                acquisition_cost = transaction_cost
+                storage = 0 + transaction_amount
+            else:
+                storage = (previous_storage := storage) + transaction_amount
+                # When selling `acquisition_cost` doesn't change but the amount
+                # held (i.e., `storage) changes:
+                if sell(transaction_cost):
+                    acquisition_cost = acquisition_cost
+                # When buying both `acquisition_cost` and `storage` are affected:
+                else: # when buying
+                    acquisition_cost = np.average(
+                        [acquisition_cost, transaction_cost],
+                        weights=[previous_storage, transaction_amount]
+                    )
+
+            acquisition_costs.append(acquisition_cost)
+
+        return -1*Series(acquisition_costs, index=df.index, name=c._AQUISITION_COST)
+
+    acquisition_series = _calculate_acquisition_cost(df_, c)
+
+    return concat([df_, acquisition_series], axis=1)
 
 
 def calculate_statistics(df: DataFrame, financial_year: int, c: Config) -> DataFrame:
@@ -35,8 +79,12 @@ def calculate_statistics(df: DataFrame, financial_year: int, c: Config) -> DataF
     sold = df_sell.loc[:, c._AMOUNT].abs().sum()
     recieved = df_sell._cost.abs().sum()
 
-    aquisition_price = (avg_buy_price := payed / bought) * sold
+    acquisition_price = (avg_buy_price := payed / bought) * sold
 
-    profit_and_loss = recieved - aquisition_price
+    profit_and_loss = recieved - acquisition_price
 
-    return recieved, aquisition_price, profit_and_loss
+    print(f"{bought=}, {payed=}")
+    print(f"{sold=}, {recieved=}")
+    print(f"{avg_buy_price=}")
+
+    return recieved, acquisition_price, profit_and_loss
